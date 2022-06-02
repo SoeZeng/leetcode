0354b9a67fc76cfcecb3a629caf9291d32516087
SoeZeng <zengyishu213@163.com>
2022-06-02 17:43:11 +0800

    528_零钱兑换Ⅱ

---
+++ src/dynamic_programming/CoinChange2_518.java
@@ -0,0 +1,52 @@
+package dynamic_programming;
+
+import java.util.Scanner;
+
+public class CoinChange2_518 {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()) {
+            int amount = Integer.parseInt(in.nextLine());
+            String str = in.nextLine();
+            String[] arr = str.split(",");
+            int[] coins = new int[arr.length];
+            for (int i = 0; i < coins.length; i++) {
+                coins[i] = Integer.parseInt(arr[i]);
+            }
+            int ans = sol.change(amount, coins);
+            System.out.println(ans);
+        }
+    }
+
+    static class Solution {
+
+        /**
+         * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+         *
+         * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+         *
+         * 假设每一种面额的硬币有无限个。
+         *
+         * 题目数据保证结果符合 32 位带符号整数。
+         *
+         * @param amount
+         * @param coins
+         * @return
+         */
+        public int change(int amount, int[] coins) {
+            int[] dp = new int[amount + 1];
+
+            dp[0] = 1;
+
+            for(int i = 0; i < coins.length; i++) {
+                for(int j = coins[i]; j <= amount; j++) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+
+            return dp[amount];
+        }
+    }
+}
