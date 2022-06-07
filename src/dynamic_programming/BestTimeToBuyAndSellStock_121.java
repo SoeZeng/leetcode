05f767cb921aa35f009a4cc5e76ea8adffd6afa1
SoeZeng <zengyishu213@163.com>
2022-06-07 10:26:23 +0800

    add desc

--- src/dynamic_programming/BestTimeToBuyAndSellStock_121.java
+++ src/dynamic_programming/BestTimeToBuyAndSellStock_121.java
@@ -19,6 +19,17 @@ public class BestTimeToBuyAndSellStock_121 {
     }
 
     static class Solution {
+
+        /**
+         * 给定一个数组 prices ，它的第i 个元素prices[i] 表示一支给定股票第 i 天的价格。
+         *
+         * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+         *
+         * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+         *
+         * @param prices
+         * @return
+         */
         public int maxProfit(int[] prices) {
             // dp[i][0] 表示第i天持有股票所得最多现金，dp[i][1]表示第i天不持有股票所得最多现金
 //            int[][] dp = new int[prices.length][2];
