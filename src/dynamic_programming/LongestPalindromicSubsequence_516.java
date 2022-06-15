2e361debffa0957f83e6ab7df7e3b53ff87cf848
SoeZeng <zengyishu213@163.com>
2022-06-15 11:25:59 +0800

    516_最长回文子序列

---
+++ src/dynamic_programming/LongestPalindromicSubsequence_516.java
@@ -0,0 +1,47 @@
+package dynamic_programming;
+
+import java.util.Scanner;
+
+public class LongestPalindromicSubsequence_516 {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()) {
+            String s = in.nextLine();
+            int res = sol.longestPalindromeSubseq(s);
+            System.out.println(res);
+        }
+    }
+
+    static class Solution {
+
+        /**
+         * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+         *
+         * @param s
+         * @return
+         */
+        public int longestPalindromeSubseq(String s) {
+            int n = s.length();
+            char[] chars = s.toCharArray();
+            int[][] dp = new int[n + 1][n + 1];
+//            int res = 1;
+
+            for(int i = n - 1; i >= 0; i--) {
+                dp[i][i] = 1;
+                for(int j = i + 1; j < n; j++) {
+                    if(chars[i] == chars[j]) {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    } else {
+                        dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                    }
+//                    if(dp[i][j] > res) res = dp[i][j];
+                }
+            }
+
+//            return res;
+            return dp[0][n - 1];
+        }
+    }
+}
