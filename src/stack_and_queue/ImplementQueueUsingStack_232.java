57829535217f44ab2603019e98e70aa06980829c
SoeZeng <zengyishu213@163.com>
2022-07-20 22:26:05 +0800

    232_用栈实现队列

---
+++ src/stack_and_queue/ImplementQueueUsingStack_232.java
@@ -0,0 +1,57 @@
+package stack_and_queue;
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStack_232 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class MyQueue {
+
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+
+        public MyQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack1.push(x);
+        }
+
+        public int pop() {
+            if(stack1.empty()) return -1;
+
+            while(!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+            int x = stack2.pop();
+
+            while(!stack2.empty()) {
+                stack1.push(stack2.pop());
+            }
+            return x;
+        }
+
+        public int peek() {
+            if(stack1.empty()) return -1;
+
+            while(!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+            int x = stack2.peek();
+
+            while(!stack2.empty()) {
+                stack1.push(stack2.pop());
+            }
+            return x;
+        }
+
+        public boolean empty() {
+            return stack1.empty();
+        }
+    }
+}
