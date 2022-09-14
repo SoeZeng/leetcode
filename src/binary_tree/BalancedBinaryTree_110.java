6dca38a6ff105b1f95ff116d4596e024c76947ee
SoeZeng <zengyishu213@163.com>
2022-09-14 23:19:44 +0800

    平衡二叉树 110

---
+++ src/binary_tree/BalancedBinaryTree_110.java
@@ -0,0 +1,33 @@
+package binary_tree;
+
+public class BalancedBinaryTree_110 {
+
+    public static void main(String[] args) {
+        Solution110 sol = new Solution110();
+    }
+}
+
+class Solution110 {
+
+    /**
+     *
+     * @param root 根节点
+     * @return 给定二叉树是否是平衡二叉树，是则返回true，否则返回false
+     */
+    public boolean isBalanced(TreeNode root) {
+        return getHighDiff(root) == -1 ? false : true;
+    }
+
+    public int getHighDiff(TreeNode root) {
+        if(root == null) return 0;
+
+        // 若左右子树不是平衡二叉树，返回-1
+        int left = getHighDiff(root.left);
+        if(left == -1) return -1;
+        int right = getHighDiff(root.right);
+        if(right == -1) return -1;
+
+        // 若以此节点为根节点的树不是平衡二叉树返回-1，否则返回树的高度
+        return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+    }
+}
