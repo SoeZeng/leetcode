1c1bdddc2921abe02209b7c49c050ab9292143c1
SoeZeng <zengyishu213@163.com>
2022-09-20 23:58:01 +0800

    257 二叉树的所有路径 add 迭代法

--- src/binary_tree/Paths_257.java
+++ src/binary_tree/Paths_257.java
@@ -2,10 +2,14 @@ package binary_tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Paths_257 {
 
     public static void main(String[] args) {
+        Solution257 sol = new Solution257();
+        TreeNode root = new TreeNode();
+        List<String> res = sol.binaryTreePaths(root);
 
     }
 }
@@ -20,8 +24,38 @@ class Solution257 {
      * @return 二叉树的所有路径
      */
     public List<String> binaryTreePaths(TreeNode root) {
+
         if(root == null) return res;
-        traverse(root);
+
+        // 递归
+//        traverse(root);
+
+        // 迭代
+        Stack<Object> stack = new Stack<>();
+        stack.push(root);
+        stack.push(root.val + "");
+
+        while(!stack.empty()) {
+            String path = (String)stack.pop();
+            TreeNode p = (TreeNode) stack.pop();
+
+            if(root.left == null && root.right == null) {
+                res.add(path);
+            }
+
+            if(p.right != null) {
+                stack.push(p.right);
+                stack.push(path + "->" + p.right.val);
+            }
+
+            if(p.left != null) {
+                stack.push(p.left);
+                stack.push(path + "->" + p.left.val);
+            }
+
+
+        }
+
         return res;
 
     }
