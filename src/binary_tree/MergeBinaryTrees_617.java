546f844ebdbc897689e5506aa4424053180aded9
SoeZeng <zengyishu213@163.com>
2022-10-17 23:56:46 +0800

    617 合并二叉树

--- src/binary_tree/MergeBinaryTrees_617.java
+++ src/binary_tree/MergeBinaryTrees_617.java
@@ -1,5 +1,7 @@
 package binary_tree;
 
+import java.util.Stack;
+
 public class MergeBinaryTrees_617 {
 
     public static void main(String[] args) {
@@ -19,10 +21,38 @@ class Solution617 {
         if(root1 == null) return root2;
         if(root2 == null) return root1;
 
-        TreeNode root = new TreeNode(root1.val + root2.val);
-        root.left = mergeTrees(root1.left, root2.left);
-        root.right = mergeTrees(root1.right, root2.right);
+        // 使用栈迭代
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root2);
+        stack.push(root1);
+        while (!stack.isEmpty()) {
+            TreeNode node1 = stack.pop();
+            TreeNode node2 = stack.pop();
+            node1.val += node2.val;
+            if (node2.right != null && node1.right != null) {
+                stack.push(node2.right);
+                stack.push(node1.right);
+            } else {
+                if (node1.right == null) {
+                    node1.right = node2.right;
+                }
+            }
+            if (node2.left != null && node1.left != null) {
+                stack.push(node2.left);
+                stack.push(node1.left);
+            } else {
+                if (node1.left == null) {
+                    node1.left = node2.left;
+                }
+            }
+        }
+        return root1;
 
-        return root;
+        // 递归
+//        TreeNode root = new TreeNode(root1.val + root2.val);
+//        root.left = mergeTrees(root1.left, root2.left);
+//        root.right = mergeTrees(root1.right, root2.right);
+//
+//        return root;
     }
 }
