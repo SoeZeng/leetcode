1501fd094fea30ce9839173f663ec95522a089e9
SoeZeng <zengyishu213@163.com>
2022-10-26 21:44:06 +0800

    revert

--- src/backtracking/Combinations_77.java
+++ src/backtracking/Combinations_77.java
@@ -13,7 +13,7 @@ public class Combinations_77 {
             int n = Integer.parseInt(in.nextLine());
             int k = Integer.parseInt(in.nextLine());
             Solution77 sol = new Solution77();
-            sol.combine(n, k)
+            sol.combine(n, k);
         }
     }
 }
