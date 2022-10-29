277aac58d09905cc29054bd1f0b0fbff67bacda6
SoeZeng <zengyishu213@163.com>
2022-10-29 15:24:55 +0800

    40 组合总和Ⅱ 使用标记数组去重

--- src/backtracking/CombinationSum2_40.java
+++ src/backtracking/CombinationSum2_40.java
@@ -15,6 +15,7 @@ public class CombinationSum2_40 {
 class Solution40 {
     List<List<Integer>> ans = new ArrayList<>();
     List<Integer> path = new LinkedList<>();
+    boolean[] used;
 
     /**
      *
@@ -24,6 +25,7 @@ class Solution40 {
      */
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
+        used = new boolean[candidates.length];
         backTracing(candidates, target, 0, 0);
         return ans;
     }
@@ -35,11 +37,22 @@ class Solution40 {
         }
 
         for(int i = startIndex; i < candidates.length && sum + candidates[i] <= target; i++) {
-            if(i > startIndex && candidates[i] == candidates[i - 1]) continue;
+            // 使用startIndex去重
+//            if(i > startIndex && candidates[i] == candidates[i - 1]) continue;
+//            path.add(candidates[i]);
+//            backTracing(candidates, target, sum + candidates[i], i + 1);
+//            path.remove(path.size() - 1);
+
+            // 使用标记数组used去重
+            if(i > 0 && candidates[i] == candidates[i -1] && used[i - 1] == false) continue;
 
             path.add(candidates[i]);
+            used[i] = true;
             backTracing(candidates, target, sum + candidates[i], i + 1);
             path.remove(path.size() - 1);
+            used[i] = false;
+
+
         }
     }
 }
