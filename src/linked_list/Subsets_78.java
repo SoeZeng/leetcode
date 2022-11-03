7d76cd9c2a3ffb6af45695bcbdcad76646808af0
SoeZeng <zengyishu213@163.com>
2022-11-03 21:35:00 +0800

    78 子集

---
+++ src/linked_list/Subsets_78.java
@@ -0,0 +1,37 @@
+package linked_list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Subsets_78 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution78 {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> subset = new LinkedList<>();
+
+    /**
+     *
+     * @param nums 元素互不相同的整数数组
+     * @return 所有子集的集合
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        backTracing(nums, 0);
+        return result;
+    }
+
+    public void backTracing(int[] nums, int startIndex) {
+        result.add(new ArrayList<>(subset));
+
+        for(int i = startIndex; i < nums.length; i++) {
+            subset.add(nums[i]);
+            backTracing(nums, i + 1);
+            subset.remove(subset.size() - 1);
+        }
+    }
+}
