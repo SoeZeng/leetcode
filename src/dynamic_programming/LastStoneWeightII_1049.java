dc2f94ac81337d89ae3210fb542ed26bcc59466b
SoeZeng <zengyishu213@163.com>
2022-11-20 19:20:44 +0800

    #round2

--- src/dynamic_programming/LastStoneWeightII_1049.java
+++ src/dynamic_programming/LastStoneWeightII_1049.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LastStoneWeightII_1049 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution1049 sol = new Solution1049();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -19,34 +19,35 @@ public class LastStoneWeightII_1049 {
         }
     }
 
-    static class Solution {
+}
 
-        /**
-         * 有一堆石头，用整数数组stones 表示。其中stones[i] 表示第 i 块石头的重量。
-         *
-         * 每一回合，从中选出任意两块石头，然后将它们一起粉碎。假设石头的重量分别为x和y，且x <= y。那么粉碎的可能结果如下：
-         *
-         * 如果x == y，那么两块石头都会被完全粉碎；
-         * 如果x != y，那么重量为x的石头将会完全粉碎，而重量为y的石头新重量为y-x。
-         * 最后，最多只会剩下一块 石头。返回此石头 最小的可能重量 。如果没有石头剩下，就返回 0。
-         *
-         * @param stones
-         * @return
-         */
-        public int lastStoneWeightII(int[] stones) {
-            int sum = 0;
-            for(int stone : stones) sum += stone;
+class Solution1049 {
 
-            int capacity = sum >> 1;
-            int[] dp = new int[capacity + 1];
+    /**
+     * 有一堆石头，用整数数组stones 表示。其中stones[i] 表示第 i 块石头的重量。
+     *
+     * 每一回合，从中选出任意两块石头，然后将它们一起粉碎。假设石头的重量分别为x和y，且x <= y。那么粉碎的可能结果如下：
+     *
+     * 如果x == y，那么两块石头都会被完全粉碎；
+     * 如果x != y，那么重量为x的石头将会完全粉碎，而重量为y的石头新重量为y-x。
+     * 最后，最多只会剩下一块 石头。返回此石头 最小的可能重量 。如果没有石头剩下，就返回 0。
+     *
+     * @param stones
+     * @return
+     */
+    public int lastStoneWeightII(int[] stones) {
+        int sum = 0;
+        for(int stone : stones) sum += stone;
 
-            for(int i = 0; i < stones.length; i++) {
-                for(int j = capacity; j >= stones[i]; j--) {
-                    dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i]);
-                }
+        int capacity = sum >> 1;
+        int[] dp = new int[capacity + 1];
+
+        for(int i = 0; i < stones.length; i++) {
+            for(int j = capacity; j >= stones[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i]);
             }
-
-            return sum - 2 * dp[capacity];
         }
+
+        return sum - 2 * dp[capacity];
     }
 }
--- src/dynamic_programming/PartitionEqualSubsetSum_416.java
+++ src/dynamic_programming/PartitionEqualSubsetSum_416.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PartitionEqualSubsetSum_416 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution416 sol = new Solution416();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -19,45 +19,46 @@ public class PartitionEqualSubsetSum_416 {
         }
     }
 
-    static class Solution {
+}
 
-        /**
-         * 给你一个 只包含正整数 的 非空 数组 nums 。
-         * 请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
-         *
-         * @param nums
-         * @return
-         */
-        public boolean canPartition(int[] nums) {
-            int sum = 0;
-            for(int num : nums) sum += num;
-            if(sum % 2 != 0) return false;
+class Solution416 {
 
-            int capacity = sum / 2;
-            int[] dp = new int[capacity + 1];
+    /**
+     * 给你一个 只包含正整数 的 非空 数组 nums 。
+     * 请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(sum % 2 != 0) return false;
 
-            for(int i = 0; i < nums.length; i++) {
-                if(nums[i] == capacity) return true;
-                for(int j = capacity; j >= nums[i]; j--) {
-                    dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
-                    if(j == capacity && dp[j] == capacity)  return true;
-                }
+        int capacity = sum / 2;
+        int[] dp = new int[capacity + 1];
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == capacity) return true;
+            for(int j = capacity; j >= nums[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+                if(j == capacity && dp[j] == capacity)  return true;
             }
-            return false;
-
-            // int sum = 0;
-            // for(int num : nums) sum += num;
-            // if(sum % 2 != 0) return false;
-
-            // int capacity = sum / 2;
-            // int[] dp = new int[capacity + 1];
-
-            // for(int i = 0; i < nums.length; i++) {
-            //     for(int j = capacity; j >= nums[i]; j--) {
-            //         dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
-            //     }
-            // }
-            // return dp[capacity] == capacity;
         }
+        return false;
+
+        // int sum = 0;
+        // for(int num : nums) sum += num;
+        // if(sum % 2 != 0) return false;
+
+        // int capacity = sum / 2;
+        // int[] dp = new int[capacity + 1];
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     for(int j = capacity; j >= nums[i]; j--) {
+        //         dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+        //     }
+        // }
+        // return dp[capacity] == capacity;
     }
 }
