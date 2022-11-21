5d23bbec5a5bfea01f267e4d3d119d3dc9a7708c
SoeZeng <zengyishu213@163.com>
2022-11-21 22:56:33 +0800

    #round2

--- src/dynamic_programming/OneAndZero_474.java
+++ src/dynamic_programming/OneAndZero_474.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class OneAndZero_474 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution474 sol = new Solution474();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -16,39 +16,38 @@ public class OneAndZero_474 {
             System.out.println(ans);
         }
     }
+}
+class Solution474 {
 
-    static class Solution {
+    /**
+     * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+     *
+     * 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
+     *
+     * @param strs
+     * @param m
+     * @param n
+     * @return
+     */
+    public int findMaxForm(String[] strs, int m, int n) {
 
-        /**
-         * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
-         *
-         * 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
-         *
-         * @param strs
-         * @param m
-         * @param n
-         * @return
-         */
-        public int findMaxForm(String[] strs, int m, int n) {
+        // dp[i][j]表示i个0，j个1时的最大子集长度
+        int[][] dp = new int[m + 1][n + 1];
 
-            // dp[i][j]表示i个0，j个1时的最大子集长度
-            int[][] dp = new int[m + 1][n + 1];
-
-            for(String str : strs) {
-                int count0 = 0;
-                int count1 = 0;
-                for(char c : str.toCharArray()) {
-                    if(c == '0') count0++;
-                    else count1++;
-                }
-                for(int i = m; i >= count0; i--) {
-                    for(int j = n; j >= count1; j--) {
-                        dp[i][j] = Math.max(dp[i][j], dp[i - count0][j - count1] + 1);
-                    }
+        for(String str : strs) { // 遍历物品
+            int count0 = 0;
+            int count1 = 0;
+            for(char c : str.toCharArray()) {
+                if(c == '0') count0++;
+                else count1++;
+            }
+            for(int i = m; i >= count0; i--) { // 遍历背包，倒序遍历避免覆盖
+                for(int j = n; j >= count1; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - count0][j - count1] + 1);
                 }
             }
-
-            return dp[m][n];
         }
+
+        return dp[m][n];
     }
 }
--- src/dynamic_programming/TargetSum_494.java
+++ src/dynamic_programming/TargetSum_494.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TargetSum_494 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution494 sol = new Solution494();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String str = in.nextLine();
@@ -20,42 +20,46 @@ public class TargetSum_494 {
         }
     }
 
-    static class Solution {
 
-        /**
-         * 给你一个整数数组 nums 和一个整数 target 。
-         *
-         * 向数组中的每个整数前添加'+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
-         *
-         * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
-         * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
-         *
-         * @param nums
-         * @param target
-         * @return
-         */
-        public int findTargetSumWays(int[] nums, int target) {
-            int sum = 0;
-            for(int i = 0; i < nums.length; i++) sum += nums[i];
+}
 
-            // left - right = target, left + right = sum -> left = (target + sum) / 2
-            if(Math.abs(target) > sum || (target + sum) % 2 == 1) return 0;
 
-            int capacity = (target + sum) / 2;
-            // dp[j]表示运算结果等于j的不同表达式数目
-            int[] dp = new int[capacity + 1];
+class Solution494 {
 
-            dp[0] = 1;
+    /**
+     * 给你一个整数数组 nums 和一个整数 target 。
+     *
+     * 向数组中的每个整数前添加'+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+     *
+     * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+     * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) sum += nums[i];
 
-            for(int i = 0; i < nums.length; i++) {
-                for(int j = capacity; j >= nums[i]; j--) {
-                    // 在求装满背包有几种方法的情况下，递推公式一般为 dp[j] += dp[j - nums[i]]
-                    dp[j] += dp[j - nums[i]];
-                }
+        // left - right = target, left + right = sum -> left = (target + sum) / 2
+        // left >= 0
+        if(Math.abs(target) > sum || (target + sum) % 2 == 1) return 0;
+
+        int capacity = (target + sum) / 2;
+        // dp[j]表示运算结果等于j的不同表达式数目
+        int[] dp = new int[capacity + 1];
+
+        dp[0] = 1;
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = capacity; j >= nums[i]; j--) {
+                // 在求装满背包有几种方法的情况下，递推公式一般为 dp[j] += dp[j - nums[i]]
+                dp[j] += dp[j - nums[i]];
             }
-
-            return dp[capacity];
-
         }
+
+        return dp[capacity];
+
     }
 }
