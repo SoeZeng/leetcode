0f7f410c3a974c730530b4aa62822bdbd91b6b83
SoeZeng <zengyishu213@163.com>
2022-11-23 21:02:08 +0800

    #round2

--- src/dynamic_programming/CombinationSum4_377.java
+++ src/dynamic_programming/CombinationSum4_377.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CombinationSum4_377 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution377 sol = new Solution377();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String str = in.nextLine();
@@ -20,26 +20,28 @@ public class CombinationSum4_377 {
         }
     }
 
-    static class Solution {
-        /**
-         * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
-         *
-         * @param nums
-         * @param target
-         * @return
-         */
-        public int combinationSum4(int[] nums, int target) {
-            int[] dp = new int[target + 1];
+}
 
-            dp[0] = 1;
+class Solution377 {
+    /**
+     * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+     *
+     * @param nums 整数数组
+     * @param target 目标和
+     * @return 满足目标和的不同组合方式，顺序不同的序列被视作不同的组合
+     */
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
 
-            for(int i = 1; i <= target; i++) {
-                for(int j = 0; j < nums.length; j++) {
-                    if(i >= nums[j]) dp[i] += dp[i - nums[j]];
-                }
+        dp[0] = 1;
+
+        // 先遍历背包后遍历物品 --> 排列
+        for(int i = 1; i <= target; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if(i >= nums[j]) dp[i] += dp[i - nums[j]];
             }
-
-            return dp[target];
         }
+
+        return dp[target];
     }
 }
