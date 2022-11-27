4124786d7e590eec9c7af6da0ac59b4958384768
SoeZeng <zengyishu213@163.com>
2022-11-27 19:22:17 +0800

    #round2

--- src/dynamic_programming/BestTimeToBuyAndSellStock3_123.java
+++ src/dynamic_programming/BestTimeToBuyAndSellStock3_123.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class BestTimeToBuyAndSellStock3_123 {
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution123 sol = new Solution123();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -17,35 +17,73 @@ public class BestTimeToBuyAndSellStock3_123 {
             System.out.println(ans);
         }
     }
+}
 
-    static class Solution {
+class Solution123 {
 
-        /**
-         * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
-         *
-         * 设计一个算法来计算你所能获取的最大利润。你最多可以完成两笔交易。
-         *
-         * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
-         *
-         * @param prices
-         * @return
-         */
-        public int maxProfit(int[] prices) {
-            // dp[i][j]，表示的是第i天，0：没有操作，1：第一次买入，2：第一次卖出，3：第二次买入，4：第二次卖出
-            int[][] dp = new int[prices.length][5];
+    /**
+     * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+     *
+     * 设计一个算法来计算你所能获取的最大利润。你最多可以完成两笔交易。
+     *
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+//        // dp[i][j]，表示的是第i天，0：没有操作，1：第一次买入，2：第一次卖出，3：第二次买入，4：第二次卖出
+//        int[][] dp = new int[prices.length][5];
+//
+//        dp[0][1] = -prices[0];
+//        dp[0][3] = -prices[0];
+//
+//        for(int i = 1; i < prices.length; i++) {
+//            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+//            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+//            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+//            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+//        }
+//
+//        // 现金最大的时候一定是卖出的状态，而两次卖出的状态现金最大一定是最后一次卖出
+//        return dp[prices.length - 1][4];
 
-            dp[0][1] = -prices[0];
-            dp[0][3] = -prices[0];
 
-            for(int i = 1; i < prices.length; i++) {
-                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
-                dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
-                dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
-                dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
-            }
+        // dp[i][j]，表示的是第i天，0：第一次持有股票，1：第一次不持有股票，2：第二次持有股票，3：第二次不持有股票
+//        int[][] dp = new int[prices.length][4];
+//
+//        dp[0][0] = -prices[0];
+//        dp[0][1] = 0;
+//        dp[0][2] = -prices[0];
+//        dp[0][3] = 0;
+//
+//        for(int i = 1; i < prices.length; i++) {
+//            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+//            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+//            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] - prices[i]);
+//            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] + prices[i]);
+//        }
+//        return dp[prices.length - 1][3];
 
-            // 现金最大的时候一定是卖出的状态，而两次卖出的状态现金最大一定是最后一次卖出
-            return dp[prices.length - 1][4];
+
+        // 空间优化
+        int[] dp = new int[4];
+
+        dp[0] = -prices[0];
+        dp[2] = -prices[0];
+
+        for(int i = 1; i <= prices.length; i++) {
+            dp[0] = Math.max(dp[0], -prices[i - 1]);
+            dp[1] = Math.max(dp[1], dp[0] + prices[i - 1]);
+            dp[2] = Math.max(dp[2], dp[1] - prices[i - 1]);
+            dp[3] = Math.max(dp[3], dp[2] + prices[i - 1]);
         }
+
+
+        return dp[3];
+
+
+
+
     }
 }
--- src/dynamic_programming/BestTimeToBuyAndSellStock4_188.java
+++ src/dynamic_programming/BestTimeToBuyAndSellStock4_188.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class BestTimeToBuyAndSellStock4_188 {
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution188 sol = new Solution188();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             int k = Integer.parseInt(in.nextLine());
@@ -19,41 +19,65 @@ public class BestTimeToBuyAndSellStock4_188 {
         }
     }
 
-    static class Solution {
+}
 
-        /**
-         * 给定一个整数数组prices ，它的第 i 个元素prices[i] 是一支给定的股票在第 i 天的价格。
-         *
-         * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
-         *
-         * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
-         *
-         * @param k
-         * @param prices
-         * @return
-         */
-        public int maxProfit(int k, int[] prices) {
-            if (prices.length == 0) return 0;
+class Solution188 {
 
-            // [天数][股票状态]
-            // 股票状态: 奇数表示第 k 次交易持有/买入, 偶数表示第 k 次交易不持有/卖出, 0 表示没有操作
-            int[][] dp = new int[prices.length][2 * k + 1];
+    /**
+     * 给定一个整数数组prices ，它的第 i 个元素prices[i] 是一支给定的股票在第 i 天的价格。
+     *
+     * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+     *
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     *
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int k, int[] prices) {
+//            if (prices.length == 0) return 0;
+//
+//            // [天数][股票状态]
+//            // 股票状态: 奇数表示第 k 次交易持有/买入, 偶数表示第 k 次交易不持有/卖出, 0 表示没有操作
+//            int[][] dp = new int[prices.length][2 * k + 1];
+//
+//            for(int i = 1; i < 2 * k; i += 2) {
+//                dp[0][i] = -prices[0];
+//            }
+//
+//            for(int i = 1; i < prices.length; i++) {
+//                // for(int j = 1; j < 2 * k + 1; j++) {
+//                //     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1] + prices[i] * (int)Math.pow(-1,j));
+//                // }
+//                for (int j = 0; j < k * 2 - 1; j += 2) {
+//                    dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+//                    dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+//                }
+//            }
+//
+//            return dp[prices.length - 1][2 * k];
 
-            for(int i = 1; i < 2 * k; i += 2) {
-                dp[0][i] = -prices[0];
+        // 空间优化
+        int n = 2 * k;
+        int[] dp = new int[n + 1];
+
+        for(int i = 0; i <= n; i++) {
+            if(i % 2 == 1) {
+                dp[i] = -prices[0];
             }
+        }
 
-            for(int i = 1; i < prices.length; i++) {
-                // for(int j = 1; j < 2 * k + 1; j++) {
-                //     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1] + prices[i] * (int)Math.pow(-1,j));
-                // }
-                for (int j = 0; j < k * 2 - 1; j += 2) {
-                    dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
-                    dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+        for(int i = 1; i <= prices.length; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(j % 2 == 1) {
+                    dp[j] = Math.max(dp[j], dp[j - 1] - prices[i - 1]);
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1] + prices[i - 1]);
                 }
             }
-
-            return dp[prices.length - 1][2 * k];
         }
+
+        return dp[n];
     }
+
 }
