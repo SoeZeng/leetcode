49b5452ab36f0dbe0ace07e8c941890077a5eeec
SoeZeng <zengyishu213@163.com>
2022-11-30 23:57:45 +0800

    #round2

--- src/dynamic_programming/LongestIncreasingSubsequence_300.java
+++ src/dynamic_programming/LongestIncreasingSubsequence_300.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class LongestIncreasingSubsequence_300 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution300 sol = new Solution300();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String str = in.nextLine();
@@ -21,34 +21,51 @@ public class LongestIncreasingSubsequence_300 {
             System.out.println(result);
         }
     }
+}
 
-    static class Solution {
+class Solution300 {
 
-        /**
-         * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
-         *
-         * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
-         *
-         * @param nums
-         * @return
-         */
-        public int lengthOfLIS(int[] nums) {
-            int result = 0;
-            int[] dp = new int[nums.length];
+    /**
+     * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+     *
+     * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+//            int result = 0;
+//            int[] dp = new int[nums.length];
+//
+//            // for(int i = 0; i < nums.length; i++) {
+//            //     dp[i] = 1;
+//            // }
+//            Arrays.fill(dp,1);
+//
+//            for(int i = 0; i < nums.length; i++) {
+//                for(int j = 0; j < i; j++) {
+//                    if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+//                }
+//                result = dp[i] > result ? dp[i] : result;
+//            }
+//
+//            return result;
 
-            // for(int i = 0; i < nums.length; i++) {
-            //     dp[i] = 1;
-            // }
-            Arrays.fill(dp,1);
 
-            for(int i = 0; i < nums.length; i++) {
-                for(int j = 0; j < i; j++) {
-                    if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+        int[] dp = new int[nums.length];
+
+        int res = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-                result = dp[i] > result ? dp[i] : result;
             }
-
-            return result;
+            res = Math.max(res, dp[i]);
         }
+
+        return res;
     }
 }
