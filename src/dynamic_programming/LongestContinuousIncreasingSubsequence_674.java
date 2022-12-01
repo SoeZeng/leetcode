1686a688923293375a6608953ad8e4dcaa5d10f9
SoeZeng <zengyishu213@163.com>
2022-12-01 22:37:33 +0800

    #round2

--- src/dynamic_programming/LongestContinuousIncreasingSubsequence_674.java
+++ src/dynamic_programming/LongestContinuousIncreasingSubsequence_674.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LongestContinuousIncreasingSubsequence_674 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution674 sol = new Solution674();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -18,41 +18,41 @@ public class LongestContinuousIncreasingSubsequence_674 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
+class Solution674 {
 
-        /**
-         * 给定一个未经排序的整数数组，找到最长且 连续递增的子序列，并返回该序列的长度。
-         *
-         * @param nums
-         * @return
-         */
-        public int findLengthOfLCIS(int[] nums) {
+    /**
+     * 给定一个未经排序的整数数组，找到最长且 连续递增的子序列，并返回该序列的长度。
+     *
+     * @param nums
+     * @return
+     */
+    public int findLengthOfLCIS(int[] nums) {
 
-            // 动规
-            // int[] dp = new int[nums.length];
+        // 动规
+        // int[] dp = new int[nums.length];
 
-            // int result = 1;
-            // Arrays.fill(dp,1);
+        // int result = 1;
+        // Arrays.fill(dp,1);
 
-            // for(int i = 1; i < nums.length; i++) {
-            //     if(nums[i] > nums[i - 1]) dp[i] = dp[i - 1] + 1;
-            //     result = Math.max(result, dp[i]);
-            // }
+        // for(int i = 1; i < nums.length; i++) {
+        //     if(nums[i] > nums[i - 1]) dp[i] = dp[i - 1] + 1;
+        //     result = Math.max(result, dp[i]);
+        // }
 
-            // return result;
+        // return result;
 
-            // 贪心
-            int res = 1;
-            int cnt = 1;
-            for(int i = 1; i < nums.length; i++) {
-                if(nums[i] > nums[i - 1]) cnt++;
-                else cnt = 1;
-                res = Math.max(res, cnt);
-            }
-
-            return res;
-
+        // 贪心
+        int res = 1;
+        int cnt = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > nums[i - 1]) cnt++;
+            else cnt = 1;
+            res = Math.max(res, cnt);
         }
+
+        return res;
+
     }
 }
--- src/dynamic_programming/MaximumLengthOfRepeatedSubarray_718.java
+++ src/dynamic_programming/MaximumLengthOfRepeatedSubarray_718.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MaximumLengthOfRepeatedSubarray_718 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution718 sol = new Solution718();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s1 = in.nextLine();
@@ -24,48 +24,48 @@ public class MaximumLengthOfRepeatedSubarray_718 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
+class Solution718 {
 
-        /**
-         * 给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。
-         *
-         * @param nums1
-         * @param nums2
-         * @return
-         */
-        public int findLength(int[] nums1, int[] nums2) {
+    /**
+     * 给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int findLength(int[] nums1, int[] nums2) {
 
-            // dp[i][j] ：以下标i - 1为结尾的A，和以下标j - 1为结尾的B，最长重复子数组长度为dp[i][j]
-            // int[][] dp = new int[nums1.length + 1][nums2.length + 1];
-            // int res = Integer.MIN_VALUE;
+        // dp[i][j] ：以下标i - 1为结尾的A，和以下标j - 1为结尾的B，最长重复子数组长度为dp[i][j]
+        // int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        // int res = Integer.MIN_VALUE;
 
-            // for(int i = 1; i <= nums1.length; i++) {
-            //     for(int j = 1; j <= nums2.length; j++) {
-            //         if(nums1[i - 1] == nums2[j - 1]) {
-            //             dp[i][j] = dp[i - 1][j - 1] + 1;
-            //         }
-            //         if(dp[i][j] > res) res = dp[i][j];
-            //     }
-            // }
+        // for(int i = 1; i <= nums1.length; i++) {
+        //     for(int j = 1; j <= nums2.length; j++) {
+        //         if(nums1[i - 1] == nums2[j - 1]) {
+        //             dp[i][j] = dp[i - 1][j - 1] + 1;
+        //         }
+        //         if(dp[i][j] > res) res = dp[i][j];
+        //     }
+        // }
 
-            // return res;
+        // return res;
 
-            // 滚动数组
-            int[] dp = new int[nums2.length + 1];
-            int res = Integer.MIN_VALUE;
+        // 滚动数组
+        int[] dp = new int[nums2.length + 1];
+        int res = Integer.MIN_VALUE;
 
-            for(int i = 1; i <= nums1.length; i++) {
-                for(int j = nums2.length; j >= 1; j--) {
-                    if(nums1[i - 1] == nums2[j - 1]) {
-                        dp[j] = dp[j - 1] + 1;
-                    } else {
-                        dp[j] = 0;
-                    }
-                    if(res < dp[j]) res = dp[j];
+        for(int i = 1; i <= nums1.length; i++) {
+            for(int j = nums2.length; j >= 1; j--) {
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = dp[j - 1] + 1;
+                } else {
+                    dp[j] = 0;
                 }
+                if(res < dp[j]) res = dp[j];
             }
-            return res;
         }
+        return res;
     }
 }
