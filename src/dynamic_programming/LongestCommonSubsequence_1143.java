bd48db0c9c4c30b8df639f80bf0bdd66853633b1
SoeZeng <zengyishu213@163.com>
2022-12-02 23:45:12 +0800

    #round2

--- src/dynamic_programming/LongestCommonSubsequence_1143.java
+++ src/dynamic_programming/LongestCommonSubsequence_1143.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LongestCommonSubsequence_1143 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution1143 sol = new Solution1143();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s1 = in.nextLine();
@@ -14,35 +14,36 @@ public class LongestCommonSubsequence_1143 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
 
-        /**
-         * 给定两个字符串text1 和text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
-         *
-         * 一个字符串的子序列是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后
-         * 组成的新字符串。
-         *
-         * @param text1
-         * @param text2
-         * @return
-         */
-        public int longestCommonSubsequence(String text1, String text2) {
-            char[] str1 = text1.toCharArray();
-            char[] str2 = text2.toCharArray();
-            int[][] dp = new int[str1.length + 1][str2.length + 1];
+class Solution1143 {
 
-            for(int i = 1; i <= str1.length; i++) {
-                for(int j = 1; j <= str2.length; j++) {
-                    if(str1[i - 1] == str2[j - 1]) {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                    } else {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                    }
+    /**
+     * 给定两个字符串text1 和text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+     *
+     * 一个字符串的子序列是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后
+     * 组成的新字符串。
+     *
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        char[] str1 = text1.toCharArray();
+        char[] str2 = text2.toCharArray();
+        int[][] dp = new int[str1.length + 1][str2.length + 1];
+
+        for(int i = 1; i <= str1.length; i++) {
+            for(int j = 1; j <= str2.length; j++) {
+                if(str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
-
-            return dp[str1.length][str2.length];
         }
+
+        return dp[str1.length][str2.length];
     }
 }
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,28 +1,68 @@
 package test;
 
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        while(in.hasNextLine()) {
-            String s = in.nextLine();
-            String t = in.nextLine();
+        List<Employee> employees = new ArrayList<>();
 
-            Solution sol = new Solution();
-            int res = sol.func(s, t);
-            System.out.println(res);
+        //write your code here......
+        employees.add(new Employee("小明", 2500));
+        employees.add(new Employee("小军", 8000));
+        employees.add(new Employee("小红", 100000));
 
+        for(Employee employee: employees) {
+            double salary = employee.getSalary();
+            salary -= 3500;
+            double res;
+            if(salary <= 0) {
+                res = 0;
+            }else if(salary <= 1500) {
+                res = salary * 0.03;
+            }else if(salary <= 4500) {
+                res = salary * 0.1 - 105;
+            }else if(salary <= 9000) {
+                res = salary * 0.2 - 555;
+            }else if(salary <= 35000) {
+                res = salary * 0.25 - 1005;
+            }else if(salary <= 55000) {
+                res = salary * 0.3 - 2755;
+            }else if(salary <= 80000) {
+                res = salary * 0.35 - 5505;
+            }else {
+                res = salary * 0.45 - 13505;
+            }
+
+            System.out.println(employee.getName() + "应该缴纳的个人所得税是：" + String.format("%.1f", res));
         }
 
-    }
-}
 
-class Solution{
-    public int func(String s, String t) {
 
     }
 }
+class Employee{
+    private String name;
+    private double salary;
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
+
+
+
+
+
+
+
+
 
 
