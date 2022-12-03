f563d7f7bcb9249b6b5f2eeaf942c049638260c2
SoeZeng <zengyishu213@163.com>
2022-12-03 16:07:08 +0800

    #round2

--- src/dynamic_programming/MaximumSubarray_53.java
+++ src/dynamic_programming/MaximumSubarray_53.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MaximumSubarray_53 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution53 sol = new Solution53();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -18,30 +18,30 @@ public class MaximumSubarray_53 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
+class Solution53 {
 
-        /**
-         * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
-         *
-         * 子数组 是数组中的一个连续部分。
-         *
-         * @param nums
-         * @return
-         */
-        public int maxSubArray(int[] nums) {
-            int[] dp = new int[nums.length];
+    /**
+     * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     *
+     * 子数组 是数组中的一个连续部分。
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
 
-            dp[0] = nums[0];
-            int res = dp[0];
+        dp[0] = nums[0];
+        int res = dp[0];
 
-            for(int i = 1; i < nums.length; i++) {
-                dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        for(int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
 
-                if(res < dp[i]) res = dp[i];
-            }
-
-            return res;
+            if(res < dp[i]) res = dp[i];
         }
+
+        return res;
     }
 }
--- src/dynamic_programming/UncrossedLines_1035.java
+++ src/dynamic_programming/UncrossedLines_1035.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UncrossedLines_1035 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution1035 sol = new Solution1035();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s1 = in.nextLine();
@@ -24,40 +24,40 @@ public class UncrossedLines_1035 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
+class Solution1035 {
 
-        /**
-         * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
-         *
-         * 现在，可以绘制一些连接两个数字 nums1[i]和 nums2[j]的直线，这些直线需要同时满足满足：
-         *
-         * nums1[i] == nums2[j]
-         * 且绘制的直线不与任何其他连线（非水平线）相交。
-         * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
-         *
-         * 以这种方法绘制线条，并返回可以绘制的最大连线数。
-         *
-         * @param nums1
-         * @param nums2
-         * @return
-         */
-        public int maxUncrossedLines(int[] nums1, int[] nums2) {
-            int n1 = nums1.length;
-            int n2 = nums2.length;
-            int[][] dp = new int[n1 + 1][n2 + 1];
+    /**
+     * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+     *
+     * 现在，可以绘制一些连接两个数字 nums1[i]和 nums2[j]的直线，这些直线需要同时满足满足：
+     *
+     * nums1[i] == nums2[j]
+     * 且绘制的直线不与任何其他连线（非水平线）相交。
+     * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+     *
+     * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[][] dp = new int[n1 + 1][n2 + 1];
 
-            for(int i = 1; i <= n1; i++) {
-                for(int j = 1; j <= n2; j++) {
-                    if(nums1[i - 1] == nums2[j - 1]) {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                    } else {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                    }
+        for(int i = 1; i <= n1; i++) {
+            for(int j = 1; j <= n2; j++) {
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
-
-            return dp[n1][n2];
         }
+
+        return dp[n1][n2];
     }
 }
