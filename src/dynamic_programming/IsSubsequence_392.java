5ddca08068eae861d28e8281aa25281eb0023970
SoeZeng <zengyishu213@163.com>
2022-12-05 23:44:48 +0800

    #round2

--- src/dynamic_programming/IsSubsequence_392.java
+++ src/dynamic_programming/IsSubsequence_392.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class IsSubsequence_392 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution392 sol = new Solution392();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -14,61 +14,61 @@ public class IsSubsequence_392 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
+class Solution392 {
 
-        /**
-         * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
-         *
-         * @param s
-         * @param t
-         * @return
-         */
-        public boolean isSubsequence(String s, String t) {
-            if(s.length() > t.length()) return false;
+    /**
+     * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() > t.length()) return false;
 
-            char[] arr1 = s.toCharArray();
-            char[] arr2 = t.toCharArray();
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
 
-            // int[][] dp = new int[arr1.length + 1][arr2.length + 1];
+        // int[][] dp = new int[arr1.length + 1][arr2.length + 1];
 
-            // // for(int i = 1; i <= arr1.length; i++) {
-            // //     for(int j = 1; j <= arr2.length; j++) {
-            // //         if(arr1[i - 1] == arr2[j - 1]) {
-            // //             dp[i][j] = dp[i - 1][j - 1] + 1;
-            // //         } else {
-            // //             dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            // //         }
-            // //     }
-            // // }
+        // // for(int i = 1; i <= arr1.length; i++) {
+        // //     for(int j = 1; j <= arr2.length; j++) {
+        // //         if(arr1[i - 1] == arr2[j - 1]) {
+        // //             dp[i][j] = dp[i - 1][j - 1] + 1;
+        // //         } else {
+        // //             dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        // //         }
+        // //     }
+        // // }
 
-            // // return dp[arr1.length][arr2.length] == arr1.length;
+        // // return dp[arr1.length][arr2.length] == arr1.length;
 
-            // for(int i = 1; i <= arr1.length; i++) {
-            //     for(int j = 1; j <= arr2.length; j++) {
-            //         if(arr1[i - 1] == arr2[j - 1]) {
-            //             dp[i][j] = dp[i - 1][j - 1] + 1;
-            //         } else {
-            //             dp[i][j] = dp[i][j - 1];
-            //         }
-            //     }
-            // }
+        // for(int i = 1; i <= arr1.length; i++) {
+        //     for(int j = 1; j <= arr2.length; j++) {
+        //         if(arr1[i - 1] == arr2[j - 1]) {
+        //             dp[i][j] = dp[i - 1][j - 1] + 1;
+        //         } else {
+        //             dp[i][j] = dp[i][j - 1];
+        //         }
+        //     }
+        // }
 
-            // return dp[arr1.length][arr2.length] == arr1.length;
+        // return dp[arr1.length][arr2.length] == arr1.length;
 
 
-            // 双指针
-            int slow = 0;
-            int fast = 0;
+        // 双指针
+        int slow = 0;
+        int fast = 0;
 
-            while(slow < arr1.length && fast < arr2.length) {
-                if(arr1[slow] == arr2[fast]) {
-                    slow++;
-                }
-                fast++;
+        while(slow < arr1.length && fast < arr2.length) {
+            if(arr1[slow] == arr2[fast]) {
+                slow++;
             }
-            return slow == arr1.length;
-
+            fast++;
         }
+        return slow == arr1.length;
+
     }
 }
