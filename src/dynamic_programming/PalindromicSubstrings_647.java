fd9f1a75da5606ddbb4527fbbe56917010e2676e
SoeZeng <zengyishu213@163.com>
2022-12-07 23:24:36 +0800

    #round2

--- src/dynamic_programming/PalindromicSubstrings_647.java
+++ src/dynamic_programming/PalindromicSubstrings_647.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PalindromicSubstrings_647 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution647 sol = new Solution647();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -13,56 +13,56 @@ public class PalindromicSubstrings_647 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
+class Solution647 {
 
-        int res = 0;
+    int res = 0;
 
-        /**
-         * 给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。
-         *
-         * @param s
-         * @return
-         */
-        public int countSubstrings(String s) {
+    /**
+     * 给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。
+     *
+     * @param s
+     * @return
+     */
+    public int countSubstrings(String s) {
 
-            // 动态规划
-            // int n = s.length();
-            // char[] chars = s.toCharArray();
-            // boolean[][] dp = new boolean[n][n];
-            // int result = 0;
+        // 动态规划
+        // int n = s.length();
+        // char[] chars = s.toCharArray();
+        // boolean[][] dp = new boolean[n][n];
+        // int result = 0;
 
-            // for(int i = n - 1; i >= 0; i--) {
-            //     for(int j = i; j < n; j++) {
-            //         if(chars[i] == chars[j] && (j - i <= 1 || dp[i + 1][j - 1])) {
-            //             dp[i][j] = true;
-            //             result++;
-            //         }
-            //     }
-            // }
+        // for(int i = n - 1; i >= 0; i--) {
+        //     for(int j = i; j < n; j++) {
+        //         if(chars[i] == chars[j] && (j - i <= 1 || dp[i + 1][j - 1])) {
+        //             dp[i][j] = true;
+        //             result++;
+        //         }
+        //     }
+        // }
 
-            // return result;
+        // return result;
 
-            // 中心扩散法
-            int n = s.length();
-            char[] chars = s.toCharArray();
+        // 中心扩散法
+        int n = s.length();
+        char[] chars = s.toCharArray();
 
 
-            for(int i = 0; i < n; i++) {
-                count(chars, i, i); // 回文串长度为奇数
-                count(chars, i, i + 1); // 回文串长度为偶数
-            }
-
-            return res;
-
+        for(int i = 0; i < n; i++) {
+            count(chars, i, i); // 回文串长度为奇数
+            count(chars, i, i + 1); // 回文串长度为偶数
         }
 
-        public void count(char[] chars, int left, int right) {
-            while(left >= 0 && right < chars.length && chars[left] == chars[right]) {
-                res++;
-                left--;
-                right++;
-            }
+        return res;
+
+    }
+
+    public void count(char[] chars, int left, int right) {
+        while(left >= 0 && right < chars.length && chars[left] == chars[right]) {
+            res++;
+            left--;
+            right++;
         }
     }
 }
