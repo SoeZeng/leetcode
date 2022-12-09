d298b3095a060b92ddf7de80ed657b8d885474c8
SoeZeng <zengyishu213@163.com>
2022-12-09 20:33:26 +0800

    #round2

--- src/dynamic_programming/PalindromicSubstrings_647.java
+++ src/dynamic_programming/PalindromicSubstrings_647.java
@@ -44,6 +44,25 @@ class Solution647 {
 
         // return result;
 
+//        boolean[][] dp = new boolean[s.length()][s.length()];
+//        int res = 0;
+//
+//        for(int i = s.length() - 1; i >= 0; i--) {
+//            for(int j = i; j < s.length(); j++) {
+//                if(s.charAt(i) == s.charAt(j)) {
+//                    if(j - i <= 1) {
+//                        res++;
+//                        dp[i][j] = true;
+//                    } else if(dp[i + 1][ j - 1]) {
+//                        res++;
+//                        dp[i][j] = true;
+//                    }
+//                }
+//            }
+//        }
+//
+//        return res;
+
         // 中心扩散法
         int n = s.length();
         char[] chars = s.toCharArray();
