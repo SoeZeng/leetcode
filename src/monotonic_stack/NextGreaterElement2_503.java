594cd7a55da484450efdb0dd6ede9b89d3cf3a75
SoeZeng <zengyishu213@163.com>
2022-12-12 23:10:34 +0800

    #round2

--- src/monotonic_stack/NextGreaterElement2_503.java
+++ src/monotonic_stack/NextGreaterElement2_503.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class NextGreaterElement2_503 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution503 sol = new Solution503();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String str = in.nextLine();
@@ -25,21 +25,22 @@ public class NextGreaterElement2_503 {
             System.out.println(ans[ans.length - 1] + "]");
         }
     }
+}
 
-    static class Solution {
+class Solution503 {
 
-        /**
-         * 定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），
-         * 返回 nums 中每个元素的 下一个更大元素 。
-         *
-         * @param nums
-         * @return
-         */
-        public int[] nextGreaterElements(int[] nums) {
-            int[] ans = new int[nums.length];
-            Arrays.fill(ans,-1);
-            Deque<Integer> stack = new LinkedList<>();
-            stack.push(0);
+    /**
+     * 定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），
+     * 返回 nums 中每个元素的 下一个更大元素 。
+     *
+     * @param nums
+     * @return
+     */
+    public int[] nextGreaterElements(int[] nums) {
+        int[] ans = new int[nums.length];
+        Arrays.fill(ans,-1);
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(0);
 
 //            for(int i = 1; i < nums.length; i++) {
 //                while(!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
@@ -55,15 +56,14 @@ public class NextGreaterElement2_503 {
 //                }
 //                stack.push(i);
 //            }
-            for(int i = 1; i < nums.length * 2; i++) {
-                while(!stack.isEmpty() && nums[i % nums.length] > nums[stack.peek()]) {
-                    ans[stack.peek()] = nums[i % nums.length];
-                    stack.pop();
-                }
-                stack.push(i % nums.length);
+        for(int i = 1; i < nums.length * 2; i++) {
+            while(!stack.isEmpty() && nums[i % nums.length] > nums[stack.peek()]) {
+                ans[stack.peek()] = nums[i % nums.length];
+                stack.pop();
             }
-
-            return ans;
+            stack.push(i % nums.length);
         }
+
+        return ans;
     }
 }
