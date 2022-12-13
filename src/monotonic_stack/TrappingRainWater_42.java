71db4b2f7266b098e638d4f633b2ab693ae84e47
SoeZeng <zengyishu213@163.com>
2022-12-13 23:03:42 +0800

    #round2

--- src/monotonic_stack/TrappingRainWater_42.java
+++ src/monotonic_stack/TrappingRainWater_42.java
@@ -36,6 +36,10 @@ public class TrappingRainWater_42 {
         //     for(int i = 1; i < height.length; i++) {
         //         while(!stack.isEmpty() && height[i] > height[stack.peek()]) {
         //             int mid = stack.peek();
+
+                       // 只要栈头小于当前元素，出栈。分为两种情况：
+                       // ①栈头元素为第一个元素，此时不可能存放雨水
+                       // ②栈头元素不是第一个元素，作为存放雨水的凹槽，需知道凹槽左边柱子的高度
         //             stack.pop();
         //             if(!stack.isEmpty()) {
         //                 int h = Math.min(height[stack.peek()], height[i]) - height[mid];
@@ -54,18 +58,20 @@ public class TrappingRainWater_42 {
         //     // 双指针
         //     int ans = 0;
 
-        //     for(int i = 1; i < height.length - 1; i++) {
-        //         int left = height[i];
-        //         int right = height[i];
-        //         for(int l = i - 1; l >= 0; l--) {
-        //             left = Math.max(left,height[l]);
-        //         }
-        //         for(int r = i + 1; r < height.length; r++) {
-        //             right = Math.max(right,height[r]);
-        //         }
-        //         int h = Math.min(left, right) - height[i];
-        //         if(h > 0) ans += h;
-        //     }
+        // 第i个柱子能蓄多少水取决于左右两边最高柱子的高度
+        // i == 0 || i == height.length - 1时无法蓄水
+//        for(int i = 1; i < height.length - 1; i++) {
+//            int left = height[i];
+//            int right = height[i];
+//            for(int j = i - 1; j >= 0; j--) {
+//                left = Math.max(left, height[j]);
+//            }
+//            for(int k = i + 1; k < height.length; k++) {
+//                right = Math.max(right, height[k]);
+//            }
+//
+//            res += Math.min(left, right) - height[i];
+//        }
 
         //     return ans;
 
