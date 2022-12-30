08ca4f68e8c63e79966b9df930bf804d9e21c8e4
SoeZeng <zengyishu213@163.com>
2022-12-30 17:21:18 +0800

    #round2

---
+++ src/extra/linkedList/LinkedListCycle_141.java
@@ -0,0 +1,25 @@
+package extra.linkedList;
+
+public class LinkedListCycle_141 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class Solution141 {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+}
