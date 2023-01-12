d770ce04f4e9c6bfa3aca735c8e70161a8980cab
SoeZeng <zengyishu213@163.com>
2023-01-12 23:54:02 +0800

    #round2

--- src/extra/dynamicProgramming/NumberOfTheLIS_673.java
+++ src/extra/dynamicProgramming/NumberOfTheLIS_673.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class NumberOfTheLIS_673 {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution673 sol = new Solution673();
         Scanner in = new Scanner(System.in);
         while(in.hasNext()) {
             String s = in.nextLine();
@@ -19,51 +19,51 @@ public class NumberOfTheLIS_673 {
             System.out.println(res);
         }
     }
+}
 
-    static class Solution {
+class Solution673 {
 
-        /**
-         * 给定一个未排序的整数数组 nums ， 返回最长递增子序列的个数 。
-         *
-         * 注意 这个数列必须是 严格 递增的。
-         *
-         * @param nums
-         * @return
-         */
-        public int findNumberOfLIS(int[] nums) {
-            int n = nums.length;
+    /**
+     * 给定一个未排序的整数数组 nums ， 返回最长递增子序列的个数 。
+     *
+     * 注意 这个数列必须是 严格 递增的。
+     *
+     * @param nums
+     * @return
+     */
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
 
-            // dp[i]表示从0到i，最长递增子序列的长度（该递增子序列一定是包括nums[i]的）
-            int[] dp = new int[n];
-            Arrays.fill(dp,1);
-            // 以nums[i]为结尾的数组，最长递增子序列的个数为count[i]（该递增子序列一定是包括nums[i]的）
-            int[] count = new int[n];
-            Arrays.fill(count,1);
-            int max = 1;
+        // dp[i]表示从0到i，最长递增子序列的长度（该递增子序列一定是包括nums[i]的）
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        // 以nums[i]为结尾的数组，最长递增子序列的个数为count[i]（该递增子序列一定是包括nums[i]的）
+        int[] count = new int[n];
+        Arrays.fill(count,1);
+        int max = 1;
 
-            for(int i = 1; i < n; i++) {
-                for(int j = 0; j < i; j++) {
-                    if(nums[i] > nums[j]) {
-                        if(dp[i] == dp[j] + 1) {
-                            count[i] += count[j];
-                        }else if(dp[i] < dp[j] + 1) {
-                            dp[i] = dp[j] + 1;
-                            count[i] = count[j];
-                        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    if(dp[i] == dp[j] + 1) {
+                        count[i] += count[j];
+                    }else if(dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
                     }
                 }
-                if(dp[i] > max) {
-                    max = dp[i];
-                }
             }
-
-            int res = 0;
-            // 统计结果
-            for(int i = 0; i < n; i++) {
-                if(dp[i] == max) res += count[i];
+            if(dp[i] > max) {
+                max = dp[i];
             }
-
-            return res;
         }
+
+        int res = 0;
+        // 统计结果
+        for(int i = 0; i < n; i++) {
+            if(dp[i] == max) res += count[i];
+        }
+
+        return res;
     }
 }
