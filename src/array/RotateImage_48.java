58f3c64edc5ea83cebe0f49f801fcbbd45ecea0c
SoeZeng <zengyishu213@163.com>
2023-02-07 22:38:05 +0800

    48 旋转图像

---
+++ src/array/RotateImage_48.java
@@ -0,0 +1,36 @@
+package array;
+
+public class RotateImage_48 {
+
+}
+
+class Solution48 {
+
+    /**
+     * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+     *
+     * @param matrix
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // 先转置
+        for(int i = 0 ; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // 再左右镜像
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+
+    }
+}
