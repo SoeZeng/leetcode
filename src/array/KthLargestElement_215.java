b232bf0b53a6786f6aa8bdac9a1b8c78fd67777d
SoeZeng <zengyishu213@163.com>
2023-02-14 22:07:45 +0800

    215 第k个最大元素

--- src/array/KthLargestElement_215.java
+++ src/array/KthLargestElement_215.java
@@ -31,8 +31,59 @@ class Solution215 {
      * @return 第k个最大元素
      */
     public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+
+        // Bubble Sort: TimeOut
+        // for(int i = n - 1; i >= 0; i--) {
+        //     for(int j = 0; j < i; j++) {
+        //         if(nums[j] > nums[j + 1]) {
+        //             int temp = nums[j];
+        //             nums[j] = nums[j + 1];
+        //             nums[j + 1] = temp;
+        //         }
+        //     }
+        // }
+
+        // Selected Sort: TimeOut
+        // for(int i = 0; i < n; i++) {
+        //     int min = i;
+        //     for(int j = i + 1; j < n; j++) {
+        //         if(nums[j] < nums[min]) {
+        //             min = j;
+        //         }
+        //     }
+        //     int temp = nums[i];
+        //     nums[i] = nums[min];
+        //     nums[min] = temp;
+        // }
+
+
+        // Quick Sort
+        // quickSort(nums, 0, n - 1);
+
         Arrays.sort(nums);
 
-        return nums[nums.length - k];
+        return nums[n - k];
+
+    }
+
+    public void quickSort(int[] nums, int start, int end) {
+        if(start >= end) return;
+
+        int left = start;
+        int right = end;
+        int k = nums[left];
+
+        while(left < right) {
+            while(left < right && nums[right] >= k) right--;
+            nums[left] = nums[right];
+            while(left < right && nums[left] <= k) left++;
+            nums[right] = nums[left];
+        }
+        nums[left] = k;
+
+        quickSort(nums, start, left - 1);
+        quickSort(nums, left + 1, end);
+
     }
 }
