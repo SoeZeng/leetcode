94e342e657a0ad05652003c5a1e7a47a7ae42d7c
SoeZeng <zengyishu213@163.com>
2023-03-15 22:45:26 +0800

    238 除自身外数组的乘积(空间优化)

--- src/array/ProductOfArrayExceptSelf_238.java
+++ src/array/ProductOfArrayExceptSelf_238.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf_238 {
 
 }
@@ -15,20 +17,37 @@ class Solution238 {
         int n = nums.length;
         int[] answer = new int[n];
 
-        int[] left = new int[n];
-        int[] right = new int[n];
+        // 空间复杂度O(n)
+//        int[] left = new int[n];
+//        int[] right = new int[n];
+//
+//        left[0] = 1;
+//        right[n - 1] = 1;
+//
+//        for(int i = 1; i < n; i++) {
+//            left[i] = left[i - 1] * nums[i - 1];
+//            right[n - i - 1] = right[n - i] * nums[n - i];
+//        }
+//
+//        for(int i = 0; i < n; i++) {
+//            answer[i] = left[i] * right[i];
+//        }
 
-        left[0] = 1;
-        right[n - 1] = 1;
 
-        for(int i = 1; i < n; i++) {
-            left[i] = left[i - 1] * nums[i - 1];
-            right[n - i - 1] = right[n - i] * nums[n - i];
-        }
+
+        // 空间复杂度O(1)
+        Arrays.fill(answer, 1);
+
+        int left = 1;
+        int right = 1;
 
         for(int i = 0; i < n; i++) {
-            answer[i] = left[i] * right[i];
+            answer[i] *= left;
+            answer[n - i - 1] *= right;
+            left *= nums[i];
+            right *= nums[n - i - 1];
         }
+
 
         return answer;
 
