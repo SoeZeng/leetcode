d39535e2595ddc606758fd0d9ab2f3d9b8888bb6
SoeZeng <zengyishu213@163.com>
2023-04-16 23:22:27 +0800

    9 回文数

---
+++ src/PalindromeNumber_9.java
@@ -0,0 +1,39 @@
+public class PalindromeNumber_9 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    /**
+     *
+     * @param x 整数 x
+     * @return 是否是回文数，若是返回true
+     */
+    public boolean isPalindrome(int x) {
+
+        // 转换成字符串
+//        String s = x + "";
+//        char[] chars = s.toCharArray();
+//
+//        for(int i = 0, j = chars.length - 1; i < j; i++, j--) {
+//            if(chars[i] != chars[j]) return false;
+//        }
+
+//        return true;
+
+        if(x < 0) return false;
+
+        int y = 0;
+        int rem = 0;
+        int quo = x;
+        while(quo != 0) {
+            rem = quo % 10;
+            y = y * 10 + rem;
+            quo /= 10;
+        }
+
+        return x == y;
+
+    }
+}
