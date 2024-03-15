e889027f506c9a9ee8aeec0b3b2e6bfcea1a2ce1
SoeZeng <zengyishu213@163.com>
2024-03-15 17:27:47 +0800

    new

--- src/main/java/findElements.java
+++ src/main/java/findElements.java
@@ -1,20 +1,13 @@
-import com.sun.source.tree.Tree;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
  * 给出一个满足下述规则的二叉树：
- *
  * root.val == 0
  * 如果 treeNode.val == x 且 treeNode.left != null，那么 treeNode.left.val == 2 * x + 1
  * 如果 treeNode.val == x 且 treeNode.right != null，那么 treeNode.right.val == 2 * x + 2
  * 现在这个二叉树受到「污染」，所有的 treeNode.val 都变成了 -1。
- *
  * 请你先还原二叉树，然后实现 FindElements 类：
- *
  * FindElements(TreeNode* root) 用受污染的二叉树初始化对象，你需要先把它还原。
  * bool find(int target) 判断目标值 target 是否存在于还原后的二叉树中并返回结果。
  */
