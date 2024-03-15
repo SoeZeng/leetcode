bbedb38324204e36f598aea70772a44af1b0222e
SoeZeng <zengyishu213@163.com>
2024-03-15 17:20:41 +0800

    new

---
+++ src/main/java/findADivisibleArrayofStrings.java
@@ -0,0 +1,25 @@
+/**
+ * 给你一个下标从 0 开始的字符串 word ，长度为 n ，由从 0 到 9 的数字组成。另给你一个正整数 m 。
+ * word 的 可整除数组 div  是一个长度为 n 的整数数组，并满足：
+ *      如果 word[0,...,i] 所表示的 数值 能被 m 整除，div[i] = 1
+ *      否则，div[i] = 0
+ * 返回 word 的可整除数组。
+ **/
+
+public class findADivisibleArrayofStrings {
+    public int[] divisibilityArray(String word, int m) {
+        int n = word.length();
+        int[] dev = new int[n];
+        long temp = 0;
+        for (int i = 0; i < n; i++) {
+            // int temp = Integer.parseInt(word.substring(0, i + 1));
+            // long temp = Long.parseLong(word.substring(0, i + 1));
+
+            // 注意数据溢出！！！将数字每次都对m取余数，取余数后再加数位不改变取余结果的
+            temp = (temp * 10 + word.charAt(i) - '0') % m;
+            dev[i] = temp % m == 0 ? 1 : 0;
+
+        }
+        return dev;
+    }
+}
---
+++ src/main/java/findElements.java
@@ -0,0 +1,76 @@
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 给出一个满足下述规则的二叉树：
+ *
+ * root.val == 0
+ * 如果 treeNode.val == x 且 treeNode.left != null，那么 treeNode.left.val == 2 * x + 1
+ * 如果 treeNode.val == x 且 treeNode.right != null，那么 treeNode.right.val == 2 * x + 2
+ * 现在这个二叉树受到「污染」，所有的 treeNode.val 都变成了 -1。
+ *
+ * 请你先还原二叉树，然后实现 FindElements 类：
+ *
+ * FindElements(TreeNode* root) 用受污染的二叉树初始化对象，你需要先把它还原。
+ * bool find(int target) 判断目标值 target 是否存在于还原后的二叉树中并返回结果。
+ */
+
+public class findElements {
+    TreeNode root;
+
+    // Method 1
+    /**
+    List<Integer> vals = new ArrayList();
+    public findElements(TreeNode root) {
+        if(root != null) {
+            root.val = 0;
+            traversingBinaryTree(root);
+        }
+        this.root = root;
+    }
+
+    public boolean find(int target) {
+        return this.vals.indexOf(target) == -1 ? false : true;
+    }
+
+    public void traversingBinaryTree(TreeNode root) {
+        int val = root.val;
+        this.vals.add(val);
+        if(root.left != null) {
+            root.left.val = val * 2 + 1;
+            traversingBinaryTree(root.left);
+        }
+        if(root.right != null) {
+            root.right.val = val * 2 + 2;
+            traversingBinaryTree(root.right);
+        }
+    }
+     **/
+
+    // Method 2
+    Set<Integer> elements = new HashSet<>();
+    public findElements(TreeNode root) {
+        dfs(root, 0);
+        this.root = root;
+    }
+
+    public boolean find(int target) {
+        return elements.contains(target);
+    }
+
+    private void dfs(TreeNode root, int val) {
+        if(root == null) return;
+
+        root.val = val;
+        this.elements.add(val);
+        dfs(root.left, val * 2 + 1);
+        dfs(root.right, val * 2 + 2);
+    }
+
+    // Method 3 不用还原树，只判断target是否存在，直接位运算解析看树里面有没有这个node就行了
+
+}
