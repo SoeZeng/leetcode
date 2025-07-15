71e9168072eb1aade1185dfd7ebd137e4b1b4f17
Soe <zengyishu213@163.com>
2025-07-15 17:23:42 +0800

    Linked List - Middle; Dual pointer

---
+++ src/nowcoder/RemoveNthFromEnd_BM9.java
@@ -0,0 +1,30 @@
+package nowcoder;
+
+public class RemoveNthFromEnd_BM9 {
+}
+
+class Solution_BM9 {
+    public ListNode removeNthFromEnd (ListNode head, int n) {
+        // write code here
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode slow = dummyNode;
+        ListNode fast = head;
+
+        int cnt = 1;
+        while(fast != null && cnt <= n) {
+            fast = fast.next;
+            cnt++;
+        }
+
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next == null ? null : slow.next.next;
+
+        return dummyNode.next;
+
+    }
+}
