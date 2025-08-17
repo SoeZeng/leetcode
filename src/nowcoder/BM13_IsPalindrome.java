2ab958d6004cac1108a8aa7a996998e6f0530bfa
Soe <zengyishu213@163.com>
2025-08-17 15:24:04 +0800

    Linked List - optimization, fast and slow pointers

--- src/nowcoder/BM13_IsPalindrome.java
+++ src/nowcoder/BM13_IsPalindrome.java
@@ -1,6 +1,7 @@
 package nowcoder;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BM13_IsPalindrome {
 
@@ -11,17 +12,49 @@ class Solution_BM13{
         // write code here
         if(head == null || head.next == null) return true;
 
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode cur = head;
-        while(cur != null) {
-            list.add(cur.val);
-            cur = cur.next;
+//        ArrayList<Integer> list = new ArrayList<>();
+//        ListNode cur = head;
+//        while(cur != null) {
+//            list.add(cur.val);
+//            cur = cur.next;
+//        }
+//
+//        for(int i = 0, j = list.size() - 1; i < j; i++, j--) {
+//            if(!list.get(i).equals(list.get(j))) return false;
+//        }
+//
+//        return true;
+
+        // dual pointer -- fast and slow pointers
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast =fast.next.next;
         }
 
-        for(int i = 0, j = list.size() - 1; i < j; i++, j--) {
-            if(!list.get(i).equals(list.get(j))) return false;
+        slow = reverse(slow);
+        fast = head;
+        while(slow != null && fast != null) {
+            if(slow.val != fast.val) return false;
+            slow = slow.next;
+            fast = fast.next;
         }
 
         return true;
+
     }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+
 }
