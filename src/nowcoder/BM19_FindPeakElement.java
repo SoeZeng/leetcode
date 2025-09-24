5a8c6400c49dc1304c5ec040b5d753dfd52809ec
Soey.Zeng <zengyishu213@163.com>
2025-09-24 18:02:37 +0800

    LinkedList-Middle, optimization

--- src/nowcoder/BM19_FindPeakElement.java
+++ src/nowcoder/BM19_FindPeakElement.java
@@ -6,14 +6,29 @@ public class BM19_FindPeakElement {
 class Solution_BM19 {
     public int findPeakElement (int[] nums) {
         // write code here
-        if(nums.length == 1 || nums[0] > nums[1] ) return 0;
-        if(nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+//        if(nums.length == 1 || nums[0] > nums[1] ) return 0;
+//        if(nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+//
+//        for (int i = 1; i < nums.length - 1; i++) {
+//            if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+//        }
+//
+//        return -1;
 
-        for (int i = 1; i < nums.length - 1; i++) {
-            if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+        // optimization
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
 
-        return -1;
+        return left;
 
     }
 }
